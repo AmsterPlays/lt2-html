9d66d0c9945ae74d8e4126d3d14d3df7669b2751
vishy-dev <56125930+vishy-dev@users.noreply.github.com>
2020-12-22 20:07:31 +0000

    update code

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ sidebarItem = document.getElementsByClassName("sidebar-item");
 for (i = 0; i < sidebarItem.length; i++) {
     sidebarItem[i].setAttribute(
         "onclick",
-        'switchTab("' + sidebarItem[i].innerText + '");'
+        "switchTab('" + sidebarItem[i].innerText + "');"
     );
 }
 
-function switchTab(tabName, event) {
+async function switchTab(tabName, event) {
     var infoPanel = document.querySelector(".main-info-panel");
     infoPanel.style.display = "none";
     var isAppTab = false;
@@ -28,22 +28,18 @@ function switchTab(tabName, event) {
         document.querySelector(".dynamicAppPanel").style.display = "flex";
         document.querySelector(".dynamicAppTitle").innerText = tabName;
         var appGrid = document.querySelector(".dynamicAppGrid");
-        var appJson = backendGetAppList(tabName);
+        var appJson = JSON.parse(await csAPI.getAppList());
+		var tabAppJson = [];
+		  appJson.forEach(function(obj) {
+			if (obj.Category == tabName) {
+				tabAppJson.push(obj);
+			}
+        });
         appGrid.innerHTML = "";
-        appJson.apps.forEach(function(obj) {
-            appGrid.innerHTML += `<div class="app-card"><img class="app-icon" src="${obj.Icon}"></img><span class="app-text">${obj.Name}</span><div class="app-btn">Install</div></div>`;
+        tabAppJson.forEach(function(obj) {
+            appGrid.innerHTML += `<div class="app-card"><img class="app-icon" src="${obj.Icon}"></img><span class="app-text">${obj.Name}</span><div class="app-btn" onclick="alert('owo');">Install</div></div>`;
         });
     }
 }
 
-function backendGetAppList() {
-    return {
-        apps: [{
-            Name: "DemoApp1",
-            Icon: "https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/48x48/apps/firefox.svg",
-            CodeName: "DemoApp",
-            Package: "https://somewebsite.com/demo.zip",
-            EXEPath: "DemoApp1\\demo.exe",
-        }, ],
-    };
-}
+
