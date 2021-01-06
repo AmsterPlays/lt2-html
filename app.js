95f7276d99b5823372ef96c69d1775e67d435054
vishy-dev <agarwal.vrishank@gmail.com>
2021-01-06 16:44:29 +0000

    push filez

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ var appTabs = [
     "Games/Emulators",
     "Other",
 ];
-var queue
+var queue;
 
-const sidebarItems = document.querySelectorAll(".sidebar-item")
+const sidebarItems = document.querySelectorAll(".sidebar-item");
 for (const item of sidebarItems) {
-  item.setAttribute("onclick", `switchTab('${item.innerText}')`);
+    item.setAttribute("onclick", `switchTab('${item.innerText}')`);
 }
 
-
 async function switchTab(tabName) {
-    console.log("called")
     var infoPanel = document.querySelector(".main-info-panel");
+    var dynamicTitle = document.querySelector(".dynamicAppTitle");
+    var dwnlPanel = document.querySelector(".dynamicDownloadsPanel");
+    dwnlPanel.style.display = "none";
     infoPanel.style.display = "none";
+    dynamicTitle.innerText = tabName;
     var isAppTab = false;
     appTabs.forEach(function(t) {
         if (tabName == t) {
@@ -25,26 +27,36 @@ async function switchTab(tabName) {
     });
     if (isAppTab) {
         document.querySelector(".dynamicAppPanel").style.display = "flex";
-        document.querySelector(".dynamicAppTitle").innerText = tabName;
         var appGrid = document.querySelector(".dynamicAppGrid");
         var appJson = JSON.parse(await csAPI.getProp("appdist"));
-		var tabAppJson = [];
-		  appJson.forEach(function(obj) {
-			if (obj.Category == tabName) {
-				tabAppJson.push(obj);
-			}
+        var tabAppJson = [];
+        appJson.forEach(function(obj) {
+            if (obj.Category == tabName) {
+                tabAppJson.push(obj);
+            }
         });
         appGrid.innerHTML = "";
         tabAppJson.forEach(function(obj) {
             appGrid.innerHTML += `<div class="app-card"><img class="app-icon" src="${obj.Icon}"></img><span class="app-text">${obj.Name}</span><div class="app-btn" onclick="alert('owo');">Install</div></div>`;
         });
+    } else if (tabName == "Downloads") {
+        dwnlPanel.style.display = "flex";
+        document.querySelector(".dynamicAppPanel").style.display = "none";
+        var dwnlGrid = document.querySelector(".dynamicDownloadsGrid");
+    }
+}
+
+function toggleDwnlPanel() {
+    var fabPanel = document.querySelector(".dwnl-panel");
+    if (fabPanel.style.display == "none") {
+        fabPanel.style.display = "flex";
+    } else {
+        fabPanel.style.display = "none";
     }
 }
 
 function reload() {
-	window.location.reload();
+    window.location.reload();
 }
 
-function installQueue(eventType, appID) {
-    
-}
+function installQueue(eventType, appID) {}
